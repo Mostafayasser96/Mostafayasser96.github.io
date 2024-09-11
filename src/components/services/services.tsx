40fd9fba7c4a8a4a497cb1962849c72e094d4db5
elhusseiny <ahmedelhusseiny2021@gmail.com>
2024-09-11 12:54:35 +0300

    added services file texts

--- src/components/services/services.tsx
+++ src/components/services/services.tsx
@@ -5,86 +5,86 @@ import React from "react";
 
 const Services = () => {
     return (
-        <div className="services">
-            <p className="services-intro">
-                في أتم جاهزية للقيام بكافة أعمال التنظيف  والتعقيم والتطهير علي مدار اليوم ..
-                تنظيف وتعقيم النوافذ والمغاسل والحمامات والجدران والارضيات والابواب
-                ازالة الغراء والاوساخ وتلميع السيراميك
-                كافة خدمات التعقيم والتطهير ومكافحة الحشرات والآفات
-            </p>
-            <div className="services-types">
-                <div className="services-part1">
-                    <div className="sub-service">
-                        <h1 className="service-name">
-                            كافة خدمات التنظيف العميق
-                        </h1>
-                        <p className="service-details">
-                            فرق مجهزة باحدث المعدات ومواد التنظيف للوصل لاعلي درجات الدقة في اعمال التنظيف والتعقيم والتطهير  باستخدام افضل مواد التنظيف مع عناية تامة لكافة مقتنياتك
-                        </p>
-                        <button className="knowMore">
-                            اعرف أكثر
-                        </button>
-                    </div>
-                    <div className="sub-service">
-                        <h1 className="service-name">
-                            كافة خدمات التنظيف العميق
-                        </h1>
-                        <p className="service-details">
-                            فرق مجهزة باحدث المعدات ومواد التنظيف للوصل لاعلي درجات الدقة في اعمال التنظيف والتعقيم والتطهير  باستخدام افضل مواد التنظيف مع عناية تامة لكافة مقتنياتك
-                        </p>
-                        <button className="knowMore">
-                            اعرف أكثر
-                        </button>
-                    </div>
-                    <div className="sub-service">
-                        <h1 className="service-name">
-                            كافة خدمات التنظيف العميق
-                        </h1>
-                        <p className="service-details">
-                            فرق مجهزة باحدث المعدات ومواد التنظيف للوصل لاعلي درجات الدقة في اعمال التنظيف والتعقيم والتطهير  باستخدام افضل مواد التنظيف مع عناية تامة لكافة مقتنياتك
-                        </p>
-                        <button className="knowMore">
-                            اعرف أكثر
-                        </button>
-                    </div>
-                </div>
-                <div className="services-part2">
-                    <div className="sub-service">
-                        <h1 className="service-name">
-                            كافة خدمات التنظيف العميق
-                        </h1>
-                        <p className="service-details">
-                            فرق مجهزة باحدث المعدات ومواد التنظيف للوصل لاعلي درجات الدقة في اعمال التنظيف والتعقيم والتطهير  باستخدام افضل مواد التنظيف مع عناية تامة لكافة مقتنياتك
-                        </p>
-                        <button className="knowMore">
-                            اعرف أكثر
-                        </button>
-                    </div>
-                    <div className="sub-service">
-                        <h1 className="service-name">
-                            كافة خدمات التنظيف العميق
-                        </h1>
-                        <p className="service-details">
-                            فرق مجهزة باحدث المعدات ومواد التنظيف للوصل لاعلي درجات الدقة في اعمال التنظيف والتعقيم والتطهير  باستخدام افضل مواد التنظيف مع عناية تامة لكافة مقتنياتك
-                        </p>
-                        <button className="knowMore">
-                            اعرف أكثر
-                        </button>
-                    </div>
-                    <div className="sub-service">
-                        <h1 className="service-name">
-                            كافة خدمات التنظيف العميق
-                        </h1>
-                        <p className="service-details">
-                            فرق مجهزة باحدث المعدات ومواد التنظيف للوصل لاعلي درجات الدقة في اعمال التنظيف والتعقيم والتطهير  باستخدام افضل مواد التنظيف مع عناية تامة لكافة مقتنياتك
-                        </p>
-                        <button className="knowMore">
-                            اعرف أكثر
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        // <div className="services">
+        //     <p className="services-intro">
+        //         في أتم جاهزية للقيام بكافة أعمال التنظيف  والتعقيم والتطهير علي مدار اليوم ..
+        //         تنظيف وتعقيم النوافذ والمغاسل والحمامات والجدران والارضيات والابواب
+        //         ازالة الغراء والاوساخ وتلميع السيراميك
+        //         كافة خدمات التعقيم والتطهير ومكافحة الحشرات والآفات
+        //     </p>
+        //     <div className="services-types">
+        //         <div className="services-part1">
+        //             <div className="sub-service">
+        //                 <h1 className="service-name">
+        //                     كافة خدمات التنظيف العميق
+        //                 </h1>
+        //                 <p className="service-details">
+        //                     فرق مجهزة باحدث المعدات ومواد التنظيف للوصل لاعلي درجات الدقة في اعمال التنظيف والتعقيم والتطهير  باستخدام افضل مواد التنظيف مع عناية تامة لكافة مقتنياتك
+        //                 </p>
+        //                 <button className="knowMore">
+        //                     اعرف أكثر
+        //                 </button>
+        //             </div>
+        //             <div className="sub-service">
+        //                 <h1 className="service-name">
+        //                     كافة خدمات التنظيف العميق
+        //                 </h1>
+        //                 <p className="service-details">
+        //                     فرق مجهزة باحدث المعدات ومواد التنظيف للوصل لاعلي درجات الدقة في اعمال التنظيف والتعقيم والتطهير  باستخدام افضل مواد التنظيف مع عناية تامة لكافة مقتنياتك
+        //                 </p>
+        //                 <button className="knowMore">
+        //                     اعرف أكثر
+        //                 </button>
+        //             </div>
+        //             <div className="sub-service">
+        //                 <h1 className="service-name">
+        //                     كافة خدمات التنظيف العميق
+        //                 </h1>
+        //                 <p className="service-details">
+        //                     فرق مجهزة باحدث المعدات ومواد التنظيف للوصل لاعلي درجات الدقة في اعمال التنظيف والتعقيم والتطهير  باستخدام افضل مواد التنظيف مع عناية تامة لكافة مقتنياتك
+        //                 </p>
+        //                 <button className="knowMore">
+        //                     اعرف أكثر
+        //                 </button>
+        //             </div>
+        //         </div>
+        //         <div className="services-part2">
+        //             <div className="sub-service">
+        //                 <h1 className="service-name">
+        //                     كافة خدمات التنظيف العميق
+        //                 </h1>
+        //                 <p className="service-details">
+        //                     فرق مجهزة باحدث المعدات ومواد التنظيف للوصل لاعلي درجات الدقة في اعمال التنظيف والتعقيم والتطهير  باستخدام افضل مواد التنظيف مع عناية تامة لكافة مقتنياتك
+        //                 </p>
+        //                 <button className="knowMore">
+        //                     اعرف أكثر
+        //                 </button>
+        //             </div>
+        //             <div className="sub-service">
+        //                 <h1 className="service-name">
+        //                     كافة خدمات التنظيف العميق
+        //                 </h1>
+        //                 <p className="service-details">
+        //                     فرق مجهزة باحدث المعدات ومواد التنظيف للوصل لاعلي درجات الدقة في اعمال التنظيف والتعقيم والتطهير  باستخدام افضل مواد التنظيف مع عناية تامة لكافة مقتنياتك
+        //                 </p>
+        //                 <button className="knowMore">
+        //                     اعرف أكثر
+        //                 </button>
+        //             </div>
+        //             <div className="sub-service">
+        //                 <h1 className="service-name">
+        //                     كافة خدمات التنظيف العميق
+        //                 </h1>
+        //                 <p className="service-details">
+        //                     فرق مجهزة باحدث المعدات ومواد التنظيف للوصل لاعلي درجات الدقة في اعمال التنظيف والتعقيم والتطهير  باستخدام افضل مواد التنظيف مع عناية تامة لكافة مقتنياتك
+        //                 </p>
+        //                 <button className="knowMore">
+        //                     اعرف أكثر
+        //                 </button>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
 
