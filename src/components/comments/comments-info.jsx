2a49354445a1eb9597038faad35ddc57c3f0c50d
elhusseiny <ahmedelhusseiny2021@gmail.com>
2024-09-19 12:47:24 +0300

    added comments

--- src/components/comments/comments-info.jsx
+++ src/components/comments/comments-info.jsx
@@ -1,32 +1,32 @@
 export const Info = [
     {
         rating: "5",
-        comment: "This is the customer's comment",
-        name: "This is the customers name"
+        comment: " أشكركم على الخدمة الممتازة التي قدمتموها! كانت العملية سلسة واحترافية للغاية، والنتائج رائعة ",
+        name: " سلمان الدوسري "
     },
     {
         rating: "5",
-        comment: "This is the customer's comment",
-        name: "This is the customers name"
+        comment: " أود أن أشكر فريقكم على الاستجابة السريعة والفعّالة لطلبي. كان العمل سريعاً ودقيقاً، والنتائج تتجاوز توقعاتي ",
+        name: " فاطمة العتيبي "
     },
     {
         rating: "5",
-        comment: "This is the customer's comment",
-        name: "This is the customers name"
+        comment: " الخدمة التي قدمتموها كانت رائعة. كل زاوية في المنزل نظيفة تماماً وكأنها جديدة. شكراً لكم على الالتزام بالجودة والاهتمام بأدق التفاصيل ",
+        name: " محمد الهاشمي "
     },
     {
         rating: "5",
-        comment: "This is the customer's comment",
-        name: "This is the customers name"
+        comment: " أعجبتني جداً دقة التزامكم بالمواعيد المحددة. كانت الخدمة في الوقت المحدد وبكل احترافية ",
+        name: " سعيد الشهري "
     },
     {
         rating: "5",
-        comment: "This is the customer's comment",
-        name: "This is the customers name"
+        comment: " لقد كنت عميلًا متكرراً لشركتكم، وكل مرة أكون أكثر إعجاباً بالخدمة التي تقدمونها ",
+        name: " نورة العنزي "
     },
     {
         rating: "5",
-        comment: "This is the customer's comment",
-        name: "This is the customers name"
+        comment: " الحقيقة أن الخدمة التي تلقيتها كانت ذات قيمة ممتازة مقابل المال ",
+        name: " عبدالله السيف "
     }
 ]
