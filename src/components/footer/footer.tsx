710fed9f6fbbc75421058040fe56760fcee9bd27
mostafa1 <mostafayasser70@yahoo.com>
2024-10-01 21:34:32 +0300

    added some features, still working on responsivity

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,45 +5,73 @@ import "../footer/footer-styles.css";
 import logo from "../header/logo-2.png";
 
 const Footer = () => {
+    const phoneNumber = "0534150198";
+    interface CallButtonProps {
+        phoneNumber: string;
+    }
+    const CallButton: React.FC<CallButtonProps> = ({ phoneNumber }) => {
+        const handleCall = () => {
+            window.location.href = `tel:${phoneNumber}`;
+        };
+
+        return (
+            <button onClick={handleCall}>
+                Call Us: {phoneNumber}
+            </button>
+        );
+    };
+
+
     return (
-        <div className="footer d-flex justify-content-between">
-            <Nav fill>
-                <Nav.Item>
-                    <Nav fill>
-                        <NavDropdown title="المزيد" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">
-                                Something else here
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Item>
-                            <Nav.Link href="/home" disabled>اتصل بنا </Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="disabled" disabled>أخر التعليقات</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="disabled" disabled>عملاؤنا
-                            </Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="disabled" disabled>
-                                الرئيسية
-                            </Nav.Link>
-                        </Nav.Item>
-                        {/* This last nav item comes from images folder */}
-                    </Nav>
-                </Nav.Item>
-            </Nav>
-            <div className="copyrights">
-                <h5 className="name"> شركة الفارس لخدمات التنظيف</h5>
-                <h6 className="rights"> حقوق النشر © 2024 جميع الحقوق محفوظة</h6>
-                <div className="logo">
-                    <img src={logo} alt="this is the footer logo" className="footer-logo-img" />
+        <div className="footer">
+            <div className="sub-footer d-lg-flex justify-content-between d-sm-block">
+                <Nav fill className="d-lg-flex justify-content-between text-sm-nowrap">
+                    <Nav.Item>
+                        <Nav fill>
+                            <NavDropdown title="المزيد" id="navbarScrollingDropdown">
+                                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action4">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action5">
+                                    Something else here
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Item>
+                                <Nav.Link href="/home" disabled>اتصل بنا </Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="disabled" disabled>أخر التعليقات</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="disabled" disabled>عملاؤنا
+                                </Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="disabled" disabled>
+                                    الرئيسية
+                                </Nav.Link>
+                            </Nav.Item>
+                            {/* This last nav item comes from images folder */}
+                        </Nav>
+                    </Nav.Item>
+                </Nav>
+                <div className="copyrights d-lg-block text-sm-center text-lg-right">
+                    <h5 className="name"> شركة الفارس للنظافة</h5>
+
+                    <div className="logo">
+                        <img src={logo} alt="this is the footer logo" className="footer-logo-img" />
+                    </div>
+                    <h6 className="rights"> حقوق النشر © 2024 جميع الحقوق محفوظة</h6>
+                </div>
+            </div>
+            <div className="communicate d-sm-flex justify-content-between">
+                <div className="dial">
+                   <CallButton phoneNumber={phoneNumber} />
+                </div>
+                <div className="whatsapp">
+                    <a href="https://wa.me/966534150198">WHATSAPP</a>
                 </div>
             </div>
         </div>
--- src/components/registeration/register.tsx
+++ src/components/registeration/register.tsx
@@ -5,7 +5,7 @@ import { Info } from "./register-info";
 import "../registeration/register-styles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
-import {sendEmail} from "./smtp"
+import {sendEmail} from "./smtp";
 
 
 const Register = () => {
@@ -17,7 +17,7 @@ const Register = () => {
 
     return (
         <div className="register-form float-md-none float-lg-none col-md-12 p-y-md-10">
-            <h1 className="call-us">اتصل بنا</h1>
+            <h1 className="call-us text-xs">اتصل بنا</h1>
             <div className="callUs-line"></div>
             <p className="covering">خدمة 24 ساعة </p>
             <p className="covering2 text-nowrap">نغطى كافة أنحاء المملكة</p>
@@ -25,19 +25,18 @@ const Register = () => {
                 <FontAwesomeIcon icon={faPhone} color="rgb(246, 144, 35)" />
                   0534150198
             </a>
-            <p className="covering3">اتصل بنا .. او اترك رسالتك ودعنا نتصل بك</p>
-            <Form className="form mx-auto">
-                
+            <h3 className="covering3 mb-8 mb-md-6">اتصل بنا .. او اترك رسالتك ودعنا نتصل بك</h3>
+            <Form className="form mx-auto mb-3 mw-100">         
                 <Form.Group className="mb-3" controlId="formBasicEmail" >
-                    <Form.Label> "*سجل رقمك للاتصال بك" </Form.Label>
+                    <Form.Label className="mt-3 mb-4"> *سجل رقمك للاتصال بك </Form.Label>
                     <Form.Control type="text"  onChange={e=>setPhone(e.target.value)} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail" >
-                    <Form.Label> "*اختر الخدمة المطلوبة" </Form.Label>
+                    <Form.Label> *اختر الخدمة المطلوبة </Form.Label>
                     <Form.Control type="text"  onChange={e=>setService(e.target.value)} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail" >
-                    <Form.Label> "اترك رسالتك" </Form.Label>
+                    <Form.Label> اترك رسالتك </Form.Label>
                     <Form.Control type="text"  onChange={e=>setMessage(e.target.value)} />
                 </Form.Group>
                 
