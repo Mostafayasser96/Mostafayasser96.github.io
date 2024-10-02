b6cbfda535d2725e1136beb9568cf4cc8d51c3be
cupcake <mohammd.alkhamisi@gmail.com>
2024-10-03 01:42:08 +0300

    add `HashRouter` and setup gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Main from './pages/main';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import ServicesPage from './pages/services-page';
 import PreviousWorksPage from './pages/previous-works-page';
 import ClientsPage from './pages/clients-page';
@@ -13,16 +13,17 @@ import CallUsPage from './pages/callUs-page';
 function App() {
   return (
     <div className="App">
-      <Routes>
-      <Route path='/' Component={Main} />
-      <Route path='/services' Component={ServicesPage} />
-      <Route path='/previousWork' Component={PreviousWorksPage} />
-      <Route path='/clients' Component={ClientsPage} />
-      <Route path='/comments' Component={CommentsPage} />
-      <Route path='/call-us' Component={CallUsPage} />
-      
-      </Routes>
-        
+      <HashRouter basename="/">
+        <Routes>
+          <Route path='/' Component={Main} />
+          <Route path='/services' Component={ServicesPage} />
+          <Route path='/previousWork' Component={PreviousWorksPage} />
+          <Route path='/clients' Component={ClientsPage} />
+          <Route path='/comments' Component={CommentsPage} />
+          <Route path='/call-us' Component={CallUsPage} />
+        </Routes>
+      </HashRouter>
+
     </div>
   );
 }
