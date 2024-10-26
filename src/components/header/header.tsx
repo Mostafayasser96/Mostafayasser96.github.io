012f901b22e8f0a95ee088be246d251c4f27f4cb
mostafa1 <mostafayasser70@yahoo.com>
2024-10-26 18:50:07 +0300

    added some changes to the header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,6 @@ import logo2 from "../header/logo-2.png";
 import { Container, Nav, Col, Row } from "react-bootstrap";
 import Navbar from 'react-bootstrap/Navbar';
 import { Info } from "./header-info";
-// import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import "../header/header-styles.css";
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -36,8 +35,8 @@ const Header = () => {
         );
     };
     return (
-        <div className="header">
-            <Navbar className="nav d-flex w-100">
+        <div className="header" style={{ backgroundPosition: '-400px -100px' }}>
+            {/* <Navbar className="nav d-flex w-100">
                 <Nav fill className="header-nav nav-fill d-none d-md-flex text-md-nowrap d-lg-flex">
                     {Info.map((page, id) => (
                         <Nav.Item key={id}>
@@ -64,7 +63,12 @@ const Header = () => {
                         ))}
                     </Nav>
                 </Offcanvas.Body>
-            </Offcanvas>
+            </Offcanvas> */}
+
+
+
+
+
             {/* <p className="header-p col-lg-7 mt-lg-auto">خصم 30% علي كافة خدمات التنظيف</p>
             <p className="header-p2 mt-lg-4">تنظيف منازل – شقق – فلل - كنب – مكيفات  مجالس – تنظيف اسطح - عزل وتنظيف خزانات المياه – مكافحة حشرات</p>
             <a className="mobile-header-number mx-auto mb-3 d-flex justify-content-evenly">
@@ -90,11 +94,11 @@ const Header = () => {
                 </Col>
             </Row> */}
 
-<Container fluid className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+            {/* <Container fluid className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
       <Row>
         <Col className="header-txt text-center">
           <p className="header-p mx-auto fs-1">خصم 30% علي كافة خدمات التنظيف</p>
-          <p className="header-p2 fs-5">
+          <p className="header-p2">
             تنظيف منازل – شقق – فلل - كنب – مكيفات  مجالس – تنظيف اسطح - عزل وتنظيف خزانات المياه – مكافحة حشرات
           </p>
           <a className="mobile-header-number mx-auto mb-3 d-flex justify-content-evenly fs-4">
@@ -102,7 +106,45 @@ const Header = () => {
           </a>
         </Col>
       </Row>
-    </Container>
+    </Container> */}
+
+
+
+
+
+
+
+
+
+            {/* <Container fluid className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+                <Row className="text-center">
+                    <Col>
+                        <p className="header-p mx-auto fs-1">خصم 30% علي كافة خدمات التنظيف</p>
+                        <p className="header-p2 fs-1">
+                            تنظيف منازل – شقق – فلل - كنب – مكيفات  مجالس – تنظيف اسطح - عزل وتنظيف خزانات المياه – مكافحة حشرات
+                        </p>
+                        <a className="mobile-header-number mx-auto mb-3 d-flex justify-content-center fs-4">
+                            <CallButton phoneNumber={phoneNumber} />
+                        </a>
+                    </Col>
+                </Row>
+            </Container> */}
+
+
+
+
+
+
+
+
+            {/* <div className="sub-header">
+                <div className="header-txt1" >
+                    <p className="newP">this is the first line</p>
+                    <p className="newP2">this is the second line </p>
+                    <button className="btn">this is button</button>
+                </div>
+
+            </div> */}
 
 
 
@@ -113,6 +155,46 @@ const Header = () => {
 
 
 
+            <div className="header">
+                <Navbar className="nav d-flex w-100">
+                    <Nav fill className="header-nav nav-fill d-none d-md-flex text-md-nowrap d-lg-flex">
+                        {Info.map((page, id) => (
+                            <Nav.Item key={id}>
+                                <Link to={page.path} className="text-center">{page.name}</Link>
+                            </Nav.Item>
+                        ))}
+                    </Nav>
+                    <button className="offCanvas-btn d-block d-md-none" onClick={handleShow}>
+                        <FontAwesomeIcon icon={faBars} style={{ fontSize: "20px" }} />
+                    </button>
+                    <Navbar.Brand href="#home" className="header-brand ms-auto">
+                        <img src={logo2} alt="logo image" className="header-logo" />
+                    </Navbar.Brand>
+                </Navbar>
+                <Offcanvas show={show} onHide={handleClose} placement="end">
+                    <Offcanvas.Header closeButton>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav fill className="d-block">
+                            {Info.map((page, id) => (
+                                <Nav.Item key={id}>
+                                    <Link to={page.path} >{page.name}</Link>
+                                </Nav.Item>
+                            ))}
+                        </Nav>
+                    </Offcanvas.Body>
+                </Offcanvas>
+            </div>
+
+            <div className="overlay">
+                <p className="header-p mx-auto fs-1">خصم 30% علي كافة خدمات التنظيف</p>
+                <p className="header-p2 fs-1">
+                    تنظيف منازل – شقق – فلل - كنب – مكيفات  مجالس – تنظيف اسطح - عزل وتنظيف خزانات المياه – مكافحة حشرات
+                </p>
+                <a className="mobile-header-number mx-auto mb-3 d-flex justify-content-center fs-4">
+                    <CallButton phoneNumber={phoneNumber} />
+                </a>
+            </div>
 
 
         </div>
